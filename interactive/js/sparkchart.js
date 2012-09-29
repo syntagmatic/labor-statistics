6e8bcf1f22fed344b64f802aeac331e800236080
syntagmatic <kai.s.chang@gmail.com>
2012-09-28 18:19:22 -0700

    time series schemas

--- interactive/js/sparkchart.js
+++ interactive/js/sparkchart.js
@@ -28,3 +28,34 @@ function sparkchart(selection) {
       .x(function(d,i) { return xscale(i); })
       .y(function(d) { return yscale(d.value); }));
 };
+
+function horizon_sparkchart(selection) {
+  var margin = {top: 1, right: 1, bottom: 1, left: 1},
+      width = 160 - margin.left - margin.right,
+      height = 32 - margin.top - margin.bottom,
+      xscale = d3.scale.linear().range([0,width]),
+      yscale = d3.scale.linear().range([height,0]);
+
+  var data = selection.data()[0].map(function(d) {
+    return {
+      value: parseFloat(d.value),
+      period: d.period,
+      year: parseInt(d.year)
+    }
+  });
+
+  xscale.domain([0, data.length-1]);
+  yscale.domain([d3.min(_.pluck(data, 'value')), d3.max(_.pluck(data, 'value'))]);
+
+  var svg = selection.append("svg")
+      .attr("width", width + margin.left + margin.right)
+      .attr("height", height + margin.top + margin.bottom)
+    .append("g")
+      .attr("transform", "translate(" + margin.left + "," + margin.top + ")");
+
+  svg.append("svg:path")
+      .attr("class", "line")
+      .attr("d", d3.svg.line()
+      .x(function(d,i) { return xscale(i); })
+      .y(function(d) { return yscale(d.value); }));
+};
